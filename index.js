26a3c469522b64dd7d3ae3f0926d85c1eb102d08
Chris Mitchell <chris.mitchell@ejgallo.com>
2019-11-05 15:33:06 -0800

    updating dev branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from DEV Lambda!'),
+        body: JSON.stringify('Hello from DEV Lambda! change something'),
     };
     return response;
 };
